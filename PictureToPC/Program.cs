ad42700e4fe94508b885f01a17532154cefdf8a8
Maik Rost <maik@rostocki.net>
2024-01-09 04:34:05 +0100

    Cleanup and dont allow to start 2 instances of program

--- PictureToPC/Program.cs
+++ PictureToPC/Program.cs
@@ -1,9 +1,25 @@
 using Forms;
+using System.Diagnostics;
 
 namespace PictureToPC
 {
     internal static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -12,6 +28,22 @@ namespace PictureToPC
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            Process[] process = Process.GetProcessesByName("PictureToPC");
+
+            // check if the process is running
+            if (process.Length >= 2)
+            {
+                // check if the window is hidden / minimized
+
+                // the window is hidden so try to restore it before setting focus.
+                ShowWindow(process[0].MainWindowHandle, ShowWindowEnum.Restore);
+                
+
+                // set user the focus to the window
+                SetForegroundWindow(process[0].MainWindowHandle);
+                return;
+            }
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
